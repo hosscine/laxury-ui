0a79a21bd27b778aeb65c3d231f22bf3a8d9605d
hosscine <futagamirentaro@gmail.com>
2019-11-04 19:11:34 +0900

    add Insight view

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,18 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Luxury Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="https://pbs.twimg.com/profile_images/938212111615893504/CWOlo6Q6_400x400.jpg"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1>Laxury Insight</h1>
       </div>
 
-      <v-spacer></v-spacer>
+      <!-- <v-spacer></v-spacer>
 
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
@@ -34,27 +27,11 @@
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
---
+++ src/views/Insight.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <h1>インサイト画面</h1>
+  </v-container>
+</template>
