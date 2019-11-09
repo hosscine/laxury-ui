54e93e61f4d08a248d58fe5743017fd8489083f9
hosscine <futagamirentaro@gmail.com>
2019-11-09 16:47:50 +0900

    update insight view

--- src/views/Insight.vue
+++ src/views/Insight.vue
@@ -1,181 +1,107 @@
 <template>
   <v-container>
     <h1>インサイト画面</h1>
-    <v-data-table
-      v-bind:headers="headers"
-      v-bind:items="desserts"
-      sort-by="calories"
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <h1>Split Items</h1>
-      </template>
-
-      <template v-slot:item.action="{ item }">
-        <v-icon small class="mr-2" @click="editItem(item)">move</v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
-      </template>
-    </v-data-table>
+    <v-row>
+      <v-col class="pr-0">
+        <RemovableTable
+          v-bind:headers="headers"
+          v-bind:items="items"
+          sort-by="amount"
+          class="elevation-1"
+        >
+          <template v-slot:top></template>
+        </RemovableTable>
+      </v-col>
+      <v-col class="pl-0">
+        <RemovableTable
+          v-bind:headers="headers"
+          v-bind:items="items"
+          sort-by="amount"
+          class="elevation-1"
+        >
+          <template v-slot:top></template>
+        </RemovableTable>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import RemovableTable from "@/components/RemovableTable.vue";
+
 export default {
+  components: {
+    RemovableTable
+  },
+
   data: () => ({
-    dialog: false,
     headers: [
       {
-        text: "Dessert (100g serving)",
+        text: "Names",
         align: "left",
         sortable: false,
         value: "name"
       },
-      { text: "Calories", value: "calories" },
-      { text: "Fat (g)", value: "fat" },
-      { text: "Carbs (g)", value: "carbs" },
-      { text: "Protein (g)", value: "protein" },
+      { text: "Dates", value: "date" },
+      { text: "Amounts", value: "amount" },
       { text: "Actions", value: "action", sortable: false }
     ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0
-    },
-    defaultItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0
-    }
+    bill: [
+      {
+        name: "Frozen Yogurt",
+        amount: 159,
+        date: 24
+      },
+      {
+        name: "Ice cream sandwich",
+        amount: 237,
+        date: 37
+      },
+      {
+        name: "Eclair",
+        amount: 262,
+        date: 23
+      },
+      {
+        name: "Cupcake",
+        amount: 305,
+        date: 67
+      },
+      {
+        name: "Gingerbread",
+        amount: 356,
+        date: 49
+      },
+      {
+        name: "Jelly bean",
+        amount: 375,
+        date: 94
+      },
+      {
+        name: "Lollipop",
+        amount: 392,
+        date: 98
+      },
+      {
+        name: "Honeycomb",
+        amount: 408,
+        date: 87
+      },
+      {
+        name: "Donut",
+        amount: 452,
+        date: 51
+      },
+      {
+        name: "KitKat",
+        amount: 518,
+        date: 65
+      }
+    ]
   }),
 
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New Item" : "Edit Item";
-    }
-  },
+  computed: {},
 
-  watch: {
-    dialog(val) {
-      val || this.close();
-    }
-  },
-
-  created() {
-    this.initialize();
-  },
-
-  methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
-        }
-      ];
-    },
-
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      const index = this.desserts.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.desserts.splice(index, 1);
-    },
-
-    close() {
-      this.dialog = false;
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      }, 300);
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-      }
-      this.close();
-    }
-  }
+  methods: {}
 };
 </script>
