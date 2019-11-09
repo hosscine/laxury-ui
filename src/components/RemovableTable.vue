adc3cfbdab82ac69e81a36e868b388897ea9dc56
hosscine <futagamirentaro@gmail.com>
2019-11-09 17:12:05 +0900

    add RemovableTable component

---
+++ src/components/RemovableTable.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-data-table v-bind:headers="headers" v-bind:items="items" class="elevation-1">
+    <template v-slot:top>
+      <v-slot name="top"></v-slot>
+    </template>
+
+    <template v-slot:item.action="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">move</v-icon>
+    </template>
+  </v-data-table>
+</template>
+
+
+<script>
+export default {
+  props: {
+    headers: Object,
+    items: Array
+  },
+  data: () => ({
+    dialog: false,
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    },
+    defaultItem: {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0
+    }
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.desserts.splice(index, 1);
+    },
+
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    }
+  }
+};
+</script>
